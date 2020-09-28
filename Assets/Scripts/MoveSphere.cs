fea1f7f68d02728d50df03ebacf4704186ad2c6d
mirmidion43 <69776783+mirmidion43@users.noreply.github.com>
2020-09-28 15:31:03 +1000

    50%

---
+++ Assets/Scripts/MoveSphere.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSphere : MonoBehaviour
+{
+    private Vector3 target = new Vector3(-3,1,0);
+    private float duration = 1.5f;
+    public Tweener tweener;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!tweener.TweenExists(transform))
+        {
+            //flip target in x axis
+            target.x = target.x * -1;
+            //add a new tween with the sphere's trranform, current transform position, target and the duration variable divided by SpeedModifier from SpeedManager
+            tweener.AddTween(transform, transform.position, target, duration/SpeedManager.SpeedModifier);
+        }
+    }
+}
---
+++ Assets/Scripts/SpeedManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedManager : MonoBehaviour
+{
+    public static float speedModifier = 1.0f;
+    public static float SpeedModifier
+    {
+        get { return speedModifier;}
+    }
+
+    public enum GameSpeed {Slow = 1, Fast = 3}
+    public static GameSpeed currentSpeedState = GameSpeed.Slow;
+    public static GameSpeed CurrentSpeedState
+    {
+        get { return currentSpeedState;}
+        set { currentSpeedState = value; speedModifier = (float)currentSpeedState;}
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
